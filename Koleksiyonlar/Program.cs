f2d630aa67df4940d3fe58ef27c65f8fb0fd8bdd
orkun pınarkaya <orkunpinarkaya1@gmail.com>
2021-05-22 23:01:09 +0300

    23.00

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"orkun","ayca","ati" };
+            //Console.WriteLine(isimler[0]);
+            List<string> isimler2 = new List<string> { "orkun", "ayca", "ati" };
+            isimler2.Add("sa");
+            foreach (var item in isimler2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.ProductName = "Masa";
+            product.CategoryId = 2;
+            product.UnitPrice = 500;
+            product.UnitsInStock = 3;
+
+            Product product1 = new Product
+            {
+                Id = 2,
+                ProductName = "sandalye",
+                CategoryId = 2,
+                UnitPrice = 5,
+                UnitsInStock = 10
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
